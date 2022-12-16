ac1cf6bf753264231f1ada0010513f50f748c92c
IrinchinovZh <exp.bur@gmail.com>
2022-12-17 00:50:13 +0800

    add

---
+++ 1/Program.cs
@@ -0,0 +1,29 @@
+﻿// task 41
+// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 
+// ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+Console.Write("Введите кол-во чисел: ");
+int n = Convert.ToInt32(Console.ReadLine()), count = 0;
+for (int i = 0; i < n; i++)
+{
+    int x = Convert.ToInt32(Console.ReadLine());
+    if (x > 0)
+        count++;
+}
+Console.WriteLine(count);
+
+// // task 41
+
+// Console.Clear();
+// int[] numbers = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+// int count = 0;
+// foreach (int element in numbers)
+// {
+//     if (element > 0)
+//         count++;
+// }
+// Console.WriteLine(count);
+
---
+++ 3/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 53: Задайте двумерный массив. Напишите программу, которая поменяет местами первую и 
+// последнюю строку массива.
+
+void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(1, 11); // [1, 10]
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void SwapFirstLastString(int[,] matrix)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        int temp = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+        matrix[matrix.GetLength(0) - 1, j] = temp;
+    }
+
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер матрицы: ");
+int[] coord = Console.ReadLine().Split(" ").Select(x => int.Parse(x)).ToArray();
+int[,] matrix = new int[coord[0], coord[1]];
+InputMatrix(matrix);
+Console.WriteLine("\n\n");
+SwapFirstLastString(matrix);
